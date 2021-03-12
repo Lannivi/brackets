55c062148207e868c81f198dc4730ca7c8355702
Lannivi <on.a.collision.course@yandex.ru>
2021-03-13 00:23:52 +0400

    feat: solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length % 2 !== 0){
+    return false;
+  }
+
+  let stack = [];  
+
+  for ( let i = 0; i < str.length; i++) {    
+      for ( let j = 0; j < bracketsConfig.length; j++) {  
+        let item = bracketsConfig[j];  
+        if (stack[stack.length-1] == item[0] && str[i] == item[1]) { 
+          stack.pop()
+        } else if (str[i] == item[0]) {
+          stack.push(str[i])
+        }
+      }
+    }
+
+  if(stack.length == 0) {
+          return true;
+  } else {
+          return false;
+  }
 }
